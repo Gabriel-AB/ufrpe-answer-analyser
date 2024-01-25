16f0835325d0311e86ad46d3d72957ef68c49418
Gabriel Barbosa <gabriel.a07b@gmail.com>
2024-01-26 00:12:53 +0100

    refactor: extract components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,38 @@
+import { ReactNode } from "react";
+
 export default function Home() {
   return (
     <main className="flex flex-col h-dvh gap-8 items-center justify-center">
       <div className="flex flex-wrap gap-8 items-center justify-center w-full">
-        <div className="flex flex-col relative w-96 h-64 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
-          <div className="sticky top-0 p-2 w-full backdrop-blur shadow">
-            Pergunta:
-          </div>
-          <span className="p-2">
-            Question Text...
-          </span>
-
-        </div>
-        <div className="flex flex-col relative w-96 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
-          <div className="sticky top-0 p-2 w-full shadow">
-            Resposta:
-          </div>
-          <textarea className="p-2 min-h-52 outline-none shadow-none resize-none"/>
-        </div>
+        <Question />
+        <Answer />
       </div>
-      <button className="bg-zinc-200 rounded-lg py-2 px-4 shadow-lg shadow-zinc-400 hover:bg-zinc-300 active:bg-zinc-400 active:translate-y-px">
-        Avaliar
-      </button>
+      <Button>Avaliar</Button>
     </main>
   );
 }
+
+function Button({children, ...props}: {children: ReactNode}) {
+  return <button children={children} {...props} className="bg-zinc-200 rounded-lg py-2 px-4 shadow-lg shadow-zinc-400 hover:bg-zinc-300 active:bg-zinc-400 active:translate-y-px"/>
+}
+
+function Answer() {
+  return <div className="flex flex-col relative w-96 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
+    <div className="sticky top-0 p-2 w-full shadow">
+      Resposta:
+    </div>
+    <textarea className="p-2 min-h-52 outline-none shadow-none resize-none" />
+  </div>;
+}
+
+function Question() {
+  return <div className="flex flex-col relative w-96 h-64 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
+    <div className="sticky top-0 p-2 w-full backdrop-blur shadow">
+      Pergunta:
+    </div>
+    <span className="p-2">
+      Question Text...
+    </span>
+  </div>;
+}
+
