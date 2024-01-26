21f5e0703ea94737519fc62c96ce0d774da57944
Gabriel Barbosa <gabriel.a07b@gmail.com>
2024-01-26 23:51:33 +0100

    feat: send request to results page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,26 +5,26 @@ import { ComponentProps } from "react";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-wrap gap-8 items-center justify-center w-full justify-items-stretch">
-        <Question text="Question Text..."/>
-        <Answer/>
-      </div>
-      <Button>Avaliar</Button>
+      <form action="/results" id="answer-form" className="flex flex-wrap gap-8 items-center justify-center w-full justify-items-stretch">
+        <Question name="question" text="Question Text..." />
+        <Answer name="answer" />
+      </form>
+      <Button form="answer-form">Avaliar</Button>
     </>
   );
 }
 
 
-function Answer({title="Resposta", ...props}: ComponentProps<"textarea">) {
+function Answer({ title = "Resposta", ...props }: ComponentProps<"textarea">) {
   return <Card className="resize-y">
     <div className="absolute top-0 w-[96%] p-2 backdrop-blur">
       Resposta:
     </div>
-    <textarea title={title} {...props} className="pt-12 p-2 min-h-full bg-inherit resize-none overflow-visible outline-none shadow-none"/>
+    <textarea title={title} {...props} className="pt-12 p-2 min-h-full bg-inherit resize-none overflow-visible outline-none shadow-none" />
   </Card>;
 }
 
-function Question({text}: {text: string}) {
+function Question({ text, name }: { text: string, name?: string }) {
   return <Card>
     <div className="sticky top-0 p-2 w-full backdrop-blur">
       Pergunta:
@@ -32,6 +32,7 @@ function Question({text}: {text: string}) {
     <span className="p-2">
       {text}
     </span>
+    <input type="text" name={name} value={text} readOnly className="hidden"/>
   </Card>;
 }
 
