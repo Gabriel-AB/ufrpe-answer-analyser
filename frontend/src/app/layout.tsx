ab30513f9c24dc26e37bf42b56f549df747f4c6a
Gabriel Barbosa <gabriel.a07b@gmail.com>
2024-01-27 00:07:17 +0100

    feat: add loading page and gradient

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.className, "bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100")}>
+      <body className={twMerge(inter.className, "bg-gradient-to-b from-neutral-50 to-neutral-300 text-neutral-900 dark:from-neutral-700 dark:to-neutral-950 dark:text-neutral-100")}>
         <main className="flex flex-col h-dvh gap-8 items-center justify-center">
           {children}
         </main>
--- frontend/src/app/results/loading.tsx
+++ frontend/src/app/results/loading.tsx
@@ -6,9 +6,8 @@ export default async function Page({ searchParams }: { searchParams: { question:
   return (
     <>
       <div className="flex items-center justify-center relative">
-        <span className="absolute size-64 animate-spin bg-gradient-to-b from-blue-400 dark:from-blue-950 rounded-full"></span>
-        {/* <div className="flex items-center justify-center rounded-full bg-gray-400 size-64 outline-8 outline-dashed outline-red-700 animate-spin"> */}
-          Carregando
+        <span className="absolute z-10 size-64 animate-spin rounded-full bg-gradient-to-b from-blue-300 to-blue-700"/>
+        <span className="absolute z-20">Carregando</span>
       </div>
     </>
   )
