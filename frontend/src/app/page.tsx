58f371022c48b28a24730981799a24d4894e58a6
Gabriel Barbosa <gabriel.a07b@gmail.com>
2024-01-26 10:06:00 +0100

    feat: style improvements

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/button";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-wrap gap-8 items-center justify-center w-full">
+      <div className="flex flex-wrap gap-8 items-center justify-center w-full bg-lime-200 justify-items-stretch">
         <Question />
         <Answer />
       </div>
@@ -14,17 +14,17 @@ export default function Home() {
 
 
 function Answer() {
-  return <div className="flex flex-col relative w-96 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
-    <div className="sticky top-0 p-2 w-full shadow">
+  return <div className="flex flex-col relative h-64 w-96 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto resize-y">
+    <div className="absolute top-0 w-[96%] p-2 backdrop-blur">
       Resposta:
     </div>
-    <textarea className="p-2 min-h-52 outline-none shadow-none resize-none" />
+    <textarea title="Resposta" className="pt-12 p-2 min-h-full bg-inherit resize-none overflow-visible outline-none shadow-none"/>
   </div>;
 }
 
 function Question() {
   return <div className="flex flex-col relative w-96 h-64 border-zinc-300 shadow-lg shadow-zinc-400 rounded-lg overflow-y-auto">
-    <div className="sticky top-0 p-2 w-full backdrop-blur shadow">
+    <div className="sticky top-0 p-2 w-full backdrop-blur">
       Pergunta:
     </div>
     <span className="p-2">
