636163e9370fa9758daf6d23692456da71717228
Gabriel Barbosa <gabriel.a07b@gmail.com>
2024-01-29 11:35:02 +0100

    feat: use cookies in frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,23 @@
 import Button from "@/components/button";
 import Card from "@/components/card";
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+
+
 
 export default function Home() {
+  async function action(formData: FormData) {
+    "use server";
+    const c = cookies();
+    c.set("question", formData.get("question") as string);
+    c.set("answer", formData.get("answer") as string);
+    return redirect("/results");
+  }
   return (
     <>
-      <form action="/results" id="answer-form" className="flex flex-wrap gap-8 items-center justify-center w-full justify-items-stretch">
-        <Question name="question" text="Question Text..." />
+      <form  action={action} id="answer-form" className="flex flex-wrap gap-8 items-center justify-center w-full justify-items-stretch">
+        <Question name="question">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at tincidunt ipsum. Maecenas mattis vestibulum condimentum. Curabitur vitae tortor molestie, varius turpis pellentesque, efficitur nibh. Praesent euismod ullamcorper finibus. Etiam in nulla euismod, sodales lectus quis, hendrerit lacus.</Question>
         <Answer name="answer" />
       </form>
       <Button form="answer-form">Avaliar</Button>
@@ -24,15 +35,15 @@ function Answer({ title = "Resposta", ...props }: ComponentProps<"textarea">) {
   </Card>;
 }
 
-function Question({ text, name }: { text: string, name?: string }) {
+function Question({ children, name }: { children: string, name?: string }) {
   return <Card>
     <div className="sticky top-0 p-2 w-full backdrop-blur">
       Pergunta:
     </div>
     <span className="p-2">
-      {text}
+      {children}
     </span>
-    <input type="text" name={name} value={text} readOnly className="hidden"/>
+    <input type="text" name={name} value={children} readOnly className="hidden"/>
   </Card>;
 }
 
--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -1,15 +1,28 @@
 "use server";
 
 import Card from '@/components/card'
+import { cookies } from 'next/headers';
 import React from 'react'
 
 
-export default async function Page({searchParams}: {searchParams: {question: string, answer: string}}) {
+
+export default async function Page() {
   // const response = await fetch("https://dummyjson.com/products/1").then(r=>r.json())
-  await new Promise(e => setTimeout(e, 8000))
+  const c = cookies();
+  const question = c.get("question")?.value;
+  const answer = c.get("answer")?.value;
+  await new Promise(e => setTimeout(e, 3000));
 
   return (
     <>
+      <div className="">
+        <span className="bg-lime-600">Pergunta:</span>
+        {question}
+      </div>
+      <div>
+        <span className="bg-teal-600">Resposta:</span>
+        {answer}
+      </div>
       <Card className="items-center justify-center">
         {/* Sua resposta tem nota {JSON.stringify(response)} */}
         Sua resposta tem nota 5
